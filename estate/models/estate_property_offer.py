af20c340662104a76a418fc30aea61f3c5a7201a
LIM mserbouti <mserbouti@saddle-choice.com>
2024-03-08 11:21:34 +0100

    CH9-inverse-function

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,7 @@
-from odoo import fields, models
+from datetime import timedelta
+
+from odoo import api, fields, models
+
 
 class Property(models.Model):
     _name = "estate.property.offer"
@@ -11,3 +14,18 @@ class Property(models.Model):
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+    validity = fields.Integer(string="Validity", default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", string="Date Deadline")
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = fields.Date.today() + timedelta(days=record.validity)
+
+    @api.depends("validity", "create_date")
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
