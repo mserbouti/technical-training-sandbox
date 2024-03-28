be04819b61fce108db529259950d6237f151837c
LIM mserbouti <mserbouti@saddle-choice.com>
2024-03-28 09:14:27 +0000

    CH14-Link Module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Estate Accounting',
+    'version': '1.0',
+    'depends': ['estate','account'],
+    'author': 'Serbouti Mohamed Amine',
+    'category': 'Technical',
+    'description': """
+    Estate Accounting
+    """,
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'license': 'LGPL-3',
+
+}
