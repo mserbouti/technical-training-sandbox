b914ac27da6f881cbe05d4191119e12f41d20d7f
LIM mserbouti <mserbouti@saddle-choice.com>
2024-03-29 11:25:41 +0000

    CH14-Invoice Creation

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -11,11 +11,23 @@ class Property(models.Model):
     def change_status_to_sold(self):
         res = super().change_status_to_sold()
         for prop in self:
-            self.env["account.move"].create(
-                {
+            invoice_vals = {
                     "partner_id": prop.buyer.id,
                     "move_type": "out_invoice",
+                    "line_ids": [
+                        Command.create({
+                            "name": prop.name,
+                            "quantity": 1,
+                            "price_unit": prop.selling_price * 0.06,
+                        }),
+                        Command.create({
+                            "name": "administrative fees",
+                            "quantity": 1,
+                            "price_unit": 100,
+                        })
+                    ],
                 }
-            )
+            invoice = prop.env["account.move"].create(invoice_vals)
+            invoice.action_post() # sans ça le commande va passer dans draft
             _logger.info("creation de la commande")
         return res
