14a132d20b806e1a54b3ee9aaac9cab9516a59de
LIM mserbouti <mserbouti@saddle-choice.com>
2024-04-15 12:07:24 +0100

    owl-comp-04

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,14 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
-  static template = "awesome_owl.playground";
-  static components = { Counter, Card };
+    static template = "awesome_owl.playground";
+    static components = { Counter, Card };
+    setup() {
+        this.value1 = "<div>some text 1</div>";
+        this.value2 = markup("<div>some text 2</div>");
+    }
 }
