940cd5132aeb7d424df7b72409a1c67f7fce1ecb
LIM mserbouti <mserbouti@saddle-choice.com>
2024-04-25 09:40:49 +0100

    owl-dash-07

--- awesome_dashboard/static/src/statistics_service.js
+++ awesome_dashboard/static/src/statistics_service.js
@@ -1,16 +1,25 @@
 /** @odoo-module */
 
 import { registry } from "@web/core/registry";
-import { memoize } from "@web/core/utils/functions";
+import { reactive } from "@odoo/owl";
 
 const statisticsService = {
-    dependencies: ["rpc"],
-    async: ["loadStatistics"],
-    start(env, { rpc }) {
-        return {
-            loadStatistics: memoize(() => rpc("/awesome_dashboard/statistics")),
-        };
-    },
+  dependencies: ["rpc"],
+  start(env, { rpc }) {
+    const statistics = reactive({ isReady: false });
+
+    async function loadData() {
+      const updates = await rpc("/awesome_dashboard/statistics");
+      Object.assign(statistics, updates, { isReady: true });
+    }
+
+    setInterval(loadData, 1 * 10 * 1000);
+    loadData();
+
+    return statistics;
+  },
 };
 
-registry.category("services").add("awesome_dashboard.statistics", statisticsService);
+registry
+  .category("services")
+  .add("awesome_dashboard.statistics", statisticsService);
