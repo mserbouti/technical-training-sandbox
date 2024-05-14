4db16df62efcc52cc49abf727b9ae0508a8b16ef
LIM mserbouti <mserbouti@saddle-choice.com>
2024-05-14 14:57:04 +0100

    fix01

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,8 +9,8 @@
     """,
     # data files always loaded at installation
     "data": [
-        "security/ir.model.access.csv",
         "security/security.xml",
+        "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_property_tag_views.xml",
         "views/estate_property_offer_views.xml",
